18dbf9d2b32d74cd27a627dc72344a5c7236d5f3
LemonHeart <lemonheart228@gmail.com>
2022-06-16 12:24:45 +0300

    v1.0.4

--- public/js/chatbot.js
+++ public/js/chatbot.js
@@ -1,7 +1,9 @@
-(function (w, d, u) {
-  const s = d.createElement('script')
-  s.async = true
-  s.src = u + '?' + (Date.now() / 60000 | 0)
-  const h = d.getElementsByTagName('script')[0]
-  h.parentNode.insertBefore(s, h)
-})(window, document, 'https://cdn-ru.bitrix24.ru/b12192492/crm/site_button/loader_2_40btsp.js')
+setTimeout(() => {
+  (function (w, d, u) {
+    const s = d.createElement('script')
+    s.async = true
+    s.src = u + '?' + (Date.now() / 60000 | 0)
+    const h = d.getElementsByTagName('script')[0]
+    h.parentNode.insertBefore(s, h)
+  })(window, document, 'https://cdn-ru.bitrix24.ru/b12192492/crm/site_button/loader_2_40btsp.js')
+}, 2000)
