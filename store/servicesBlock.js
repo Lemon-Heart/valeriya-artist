77cbf983100f00bb9b788eef3735039a047e972e
LemonHeart <lemonheart228@gmail.com>
2022-06-20 22:09:17 +0300

    v1.0.28

--- store/servicesBlock.js
+++ store/servicesBlock.js
@@ -1,14 +1,14 @@
 import { reactive } from 'vue'
 
-// import { makeRequest } from '@/use/api'
-// makeRequest.get('/mock/services.json').then(data => {
-//   servicesBlock.head = data.head // Устанавливает заголовок в блоке анализов на главной странице
-//   servicesBlock.subhead = data.subhead // Устанавливает подзаголовок в блоке анализов на главной странице
-//   servicesBlock.items = data.items // Устанавливает список анализов
-// })
+import { makeRequest } from '@/use/api'
+makeRequest.get('/mock/services.json').then(data => {
+  servicesBlock.head = data.head // Устанавливает заголовок в блоке анализов на главной странице
+  servicesBlock.subhead = data.subhead // Устанавливает подзаголовок в блоке анализов на главной странице
+  servicesBlock.items = data.items // Устанавливает список анализов
+})
 
 export const servicesBlock = reactive({
   head: null,
   subhead: null,
-  items: null
+  items: []
 })
