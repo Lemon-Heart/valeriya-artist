51439d5b409e9de54147dfdfc248e8bdb50b2ff5
andrei.ibragimov <fas142acav1@gmail.com>
2023-03-02 17:41:13 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -8,12 +8,6 @@ module.exports = {
   },
   pwa: {
     name: 'Валерия',
-    themeColor: '#000000',
-    icons: [
-      { src: './img/icons/android-chrome-192x192.PNG', sizes: '192x192', type: 'image/png' },
-      { src: './img/icons/android-chrome-512x512.PNG', sizes: '512x512', type: 'image/png' },
-      { src: './img/icons/android-chrome-maskable-192x192.PNG', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
-      { src: './img/icons/android-chrome-maskable-512x512.PNG', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
-    ]
+    themeColor: '#000000'
   }
 }
