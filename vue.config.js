984fced80d2c612f08b9574d75f1e211e9602d5a
andrei.ibragimov <fas142acav1@gmail.com>
2023-03-02 17:34:10 +0300

    fix

--- vue.config.js
+++ vue.config.js
@@ -10,10 +10,10 @@ module.exports = {
     name: 'Валерия',
     themeColor: '#000000',
     icons: [
-      { src: './img/icons/android-chrome-192x192.png', sizes: '192x192', type: 'image/png' },
-      { src: './img/icons/android-chrome-512x512.png', sizes: '512x512', type: 'image/png' },
-      { src: './img/icons/android-chrome-maskable-192x192.png', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
-      { src: './img/icons/android-chrome-maskable-512x512.png', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
+      { src: './img/icons/android-chrome-192x192.PNG', sizes: '192x192', type: 'image/png' },
+      { src: './img/icons/android-chrome-512x512.PNG', sizes: '512x512', type: 'image/png' },
+      { src: './img/icons/android-chrome-maskable-192x192.PNG', sizes: '192x192', type: 'image/png', purpose: 'maskable' },
+      { src: './img/icons/android-chrome-maskable-512x512.PNG', sizes: '512x512', type: 'image/png', purpose: 'maskable' }
     ]
   }
 }
